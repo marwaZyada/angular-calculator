e4c9b255daec648656c1be3041a7d813c49cc7f6
marwaZyada <zyadamarwa6@gmail.com>
2023-06-04 01:31:38 +0300

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+ data = '';
+
+
+  display(item:any){
+    console.log(item);
+    this.data+=item
+    
+  }
+
+  clearScreen(){
+    this.data=''
+  }
+
+
+  calculate(){
+    try{
+    this.data=eval(this.data).toFixed(4)}
+catch(eror){
+this.data='error'
+};
+
+  }
+
 }
